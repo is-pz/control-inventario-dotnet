be6c0a989e27e6b5567264303d340de0c021b87b
Is-Pz <israel_paz@msn.com>
2023-04-02 16:57:08 -0500

    Agregando migraciones

---
+++ Data/Migrations/20230402215640_InitialCreated.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace control_inventario.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: false),
+                    Description = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: false),
+                    CostPrice = table.Column<decimal>(type: "decimal(2,0)", nullable: false),
+                    SalePrice = table.Column<decimal>(type: "decimal(2,0)", nullable: false),
+                    QuantityStock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "varchar(150)", unicode: false, maxLength: 150, nullable: true),
+                    IdStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Roles_Status",
+                        column: x => x.IdStatus,
+                        principalTable: "Status",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Username = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "varchar(250)", unicode: false, maxLength: 250, nullable: false),
+                    IdRol = table.Column<int>(type: "int", nullable: false),
+                    IdStatus = table.Column<int>(type: "int", nullable: false),
+                    LastLogin = table.Column<DateTime>(type: "datetime", nullable: true),
+                    CreatedAt = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Roles",
+                        column: x => x.IdRol,
+                        principalTable: "Roles",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Users_Status",
+                        column: x => x.IdStatus,
+                        principalTable: "Status",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    IdUser = table.Column<int>(type: "int", nullable: false),
+                    TotalSale = table.Column<decimal>(type: "decimal(2,0)", nullable: false),
+                    SoleAt = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sales_Users",
+                        column: x => x.IdUser,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalesItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    IdSale = table.Column<int>(type: "int", nullable: false),
+                    IdProduct = table.Column<int>(type: "int", nullable: false),
+                    SalePrice = table.Column<decimal>(type: "decimal(2,0)", nullable: false),
+                    QuantitySold = table.Column<int>(type: "int", nullable: false),
+                    Total = table.Column<decimal>(type: "decimal(2,0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SalesItems_Products",
+                        column: x => x.IdProduct,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_SalesItems_Sales",
+                        column: x => x.IdSale,
+                        principalTable: "Sales",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_IdStatus",
+                table: "Roles",
+                column: "IdStatus");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_IdUser",
+                table: "Sales",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesItems_IdProduct",
+                table: "SalesItems",
+                column: "IdProduct");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesItems_IdSale",
+                table: "SalesItems",
+                column: "IdSale");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IdRol",
+                table: "Users",
+                column: "IdRol");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IdStatus",
+                table: "Users",
+                column: "IdStatus");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesItems");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Status");
+        }
+    }
+}
