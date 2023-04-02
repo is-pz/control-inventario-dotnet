64c73a6853bf1e31be4ffd7a502d2527a3d2e776
Is-Pz <israel_paz@msn.com>
2023-04-02 15:56:38 -0500

    Correcion campo de status en modelos

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -6,7 +6,7 @@
         public string Username { get; set; } = null!;
         public string Password { get; set; } = null!;
         public int Id_Rol { get; set; }
-        public int Status { get; set; }
+        public int IdStatus { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime LastLogin { get; set;  }
 
