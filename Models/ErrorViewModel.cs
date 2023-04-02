eb4f95c43e339683b5fce2df9def39cdb633a2a0
Is-Pz <israel_paz@msn.com>
2023-04-02 15:56:52 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace control_inventario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
